62d3205578dfd192f2d88396f9bcb5c33499c71b
enoch tamulonis <yungboiiindigo@gmail.com>
2024-04-02 09:59:02 -0500

    add del

--- app/controllers/projects/drum_patterns_controller.rb
+++ app/controllers/projects/drum_patterns_controller.rb
@@ -5,6 +5,13 @@ module Projects
       @drum_pattern = @project.drum_patterns.last || @project.drum_patterns.create
     end
 
+    def destroy
+      @drum_pattern = @project.drum_patterns.find(params[:id])
+      @blob = ActiveStorage::Blob.find_signed(params[:signed_id])
+      @drum_pattern.files.find { |f| f.blob.signed_id == params[:signed_id] }.purge
+      render turbo_stream: turbo_stream.remove("blob-#{params[:signed_id].parameterize}")
+    end
+
     private
 
     def set_project
--- app/controllers/projects/file_uploads_controller.rb
+++ app/controllers/projects/file_uploads_controller.rb
@@ -6,6 +6,7 @@ module Projects
       drum_pattern = project.drum_patterns.find(params[:drum_pattern_id])
       thefile = ActiveStorage::Blob.find_signed(params[:blob_signed_id])
       drum_pattern.files.attach(thefile)
+      render turbo_stream: turbo_stream.append("tracks", partial: "projects/drum_patterns/track", locals: { sound: drum_pattern.files.last, drum_pattern: drum_pattern })
     end
   end
 end
